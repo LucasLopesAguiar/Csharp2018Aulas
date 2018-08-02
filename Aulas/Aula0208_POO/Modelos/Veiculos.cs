a101defdf5d0dd6d1395771d22ceb623f8c6b3a7
LeonanML <leonan.ml97@gmail.com>
2018-08-02 20:35:35 -0300

    Criação de Classes Carro, Moto, Veiculos

---
+++ Aulas/Aula0208_POO/Modelos/Veiculos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0208_POO.Modelos
+{
+    class Veiculos
+    {
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+
+        private string marca;
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+    }
+}
