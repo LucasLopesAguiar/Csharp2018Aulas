6ea8f18f849c50494ddde591c95a2a333e2b0aad
LeonanML <leonan.ml97@gmail.com>
2018-08-16 21:26:09 -0300

    SQL

---
+++ Aulas/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿RESTORE FILELISTONLY
+FROM DISK = 'C:\Users\1616696\AdventureWorks2016.bak'
+
+RESTORE DATABASE AdventureWorks2016
+FROM DISK = 'C:\Users\1616696\AdventureWorks2016.bak'
+
+WITH MOVE 'AdventureWorks2016_data' TO 'C:\Users\1616696\Source\Repos\Csharp2018Aulas\DB\AdventureWorks2016.mdf',
+MOVE 'AdventureWorks2016_log' TO 'C:\Users\1616696\Source\Repos\Csharp2018Aulas\DB\AdventureWorks2016.ldf',
+REPLACE;
