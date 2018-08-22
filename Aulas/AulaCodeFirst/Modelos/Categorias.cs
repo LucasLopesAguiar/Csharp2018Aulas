6c4639976e6a24209e81868e70fbcbcaed36ea40
LeonanML <leonan.ml97@gmail.com>
2018-08-21 21:31:55 -0300

    Escrevendo estrutura da classe categorias e atribuindo a chave primaria

--- Aulas/AulaCodeFirst/Modelos/Categorias.cs
+++ Aulas/AulaCodeFirst/Modelos/Categorias.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace AulaCodeFirst.Modelos
     class Categorias
     {
 
+        [Key]
         public int CategoriaID { get; set; }
         public String Nome { get; set; }
         public String Descricao { get; set; }
