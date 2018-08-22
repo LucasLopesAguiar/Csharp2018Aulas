76cc417d640c5bc0d501fc6f3fff6de701e93b2d
LucasLopesAguiar <lucaslopesdeaguiar@outlook.com>
2018-08-21 21:31:53 -0300

    Escrevendo Estrutura da classe categoria

--- Aulas/AulaCodeFirst/Modelos/Categoria.cs
+++ Aulas/AulaCodeFirst/Modelos/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace AulaCodeFirst.Modelos
 {
  public class Categoria
     {
+        [Key]
+        public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
     }
 }
