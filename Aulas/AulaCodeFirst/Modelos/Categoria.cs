dedf1811e7718c817978796c5bf25bbef9f9d648
LucasLopesAguiar <lucaslopesdeaguiar@outlook.com>
2018-08-21 21:52:50 -0300

    Relacionament Categoria ---> Produto e DataAnotations (Required,StringLength)

--- Aulas/AulaCodeFirst/Modelos/Categoria.cs
+++ Aulas/AulaCodeFirst/Modelos/Categoria.cs
@@ -1,18 +1,26 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AulaCodeFirst.Modelos
 {
- public class Categoria
+    public class Categoria
     {
         [Key]
         public int CategoriaID { get; set; }
+
+        [Required]
+        [StringLength(49)]
         public string Nome { get; set; }
         public string Descricao { get; set; }
+
+
+        // Relacionamento Categoria <-------->
+
+        public virtual ICollection<Produto> Produtos { get; set; }
+                                           
+
+
 
     }
 }
