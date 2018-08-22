a0b46c424449f9c1d3dee1f62abdbc2f7c0f0015
LeonanML <leonan.ml97@gmail.com>
2018-08-21 21:28:20 -0300

    Criação da classe Categorias

---
+++ Aulas/AulaCodeFirst/Modelos/Categorias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaCodeFirst.Modelos
+{
+    class Categorias
+    {
+
+        public int CategoriaID { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+
+
+    }
+}
